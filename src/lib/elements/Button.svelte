29cdd882132275fd04a84ad7ec35e9a933ead7c4
Fredrik Kvalvik <fredrik.kvalvik@gmail.com>
2022-07-02 18:06:04 +0200

    update button element

--- src/lib/elements/Button.svelte
+++ src/lib/elements/Button.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 
-  export let disabled: boolean | undefined
-
+export let disabled: boolean | undefined
 </script>
 
 <button {disabled}
 class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+class:disabled
 on:click><slot/></button
 > 
+
+<style>
+  .disabled {
+    @apply bg-blue-900 text-gray-400 cursor-wait;
+  }
+</style>
